ad0631c239e85bfd148ac4b47a6dc1c8a420bd1c
VaaniniBansal <76036181+VaaniniBansal@users.noreply.github.com>
2022-06-10 15:53:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var a1, ground;
+var m1;
+
+function preload(){
+  spaceshipDark=loadImage("images/spaceship dark.png");
+
+}
+
+function setup(){
+    var canvas = createCanvas(displayWidth,displayHeight);
+    engine = Engine.create();
+    world = engine.world;
+    a1= new Astro(100,20,100,200);
+    ground= new Ground(800,height,1600,29);
+    m1= new Maze(width/2,600,width,100);
+}                  
+
+function draw(){
+    background(spaceshipDark);
+    Engine.update(engine);
+    a1.display();
+    ground.display();
+    m1.display();
+}
