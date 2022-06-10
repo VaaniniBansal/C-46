e9845ef5260f15da49754e89aa0b011d82bd5b87
VaaniniBansal <76036181+VaaniniBansal@users.noreply.github.com>
2022-06-10 15:58:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,9 +14,10 @@ function setup(){
     var canvas = createCanvas(displayWidth,displayHeight);
     engine = Engine.create();
     world = engine.world;
-    a1= new Astro(100,20,100,200);
+    a1= new Astro(100,20,50,100);
     ground= new Ground(800,height,1600,29);
-    m1= new Maze(width/2,600,width,100);
+   // m1= new Maze(width/2,600,width,100);
+    m1= new Ground(100,100,1000,10);
 }                  
 
 function draw(){
